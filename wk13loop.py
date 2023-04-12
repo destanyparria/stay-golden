cdebd96d785ce76c0da656dfff40f59d7b237d68
Destany Parria <destanyparria@gmail.com>
2023-04-12 07:35:55 +0000

    Dictionary List

---
+++ wk13loop.py
@@ -0,0 +1,26 @@
+import os 
+
+# Get current working directory
+cwd = os.getcwd( )
+
+# Creates an empty list 
+dict_list = [ ] 
+
+for file_name in os.listdir(cwd):
+    file_path = os.path.join(cwd, file_name) 
+    
+    file_size = os.path.getsize(file_path)
+    
+    dict_file = {
+        'file_name' : file_name,
+        'file_path' : file_path,
+        'file_size' : file_size
+    }
+    
+    # Add the dictionary to the list
+    dict_list.append(dict_file)
+    
+    
+    
+    print(dict_list, sep="\n") 
+
